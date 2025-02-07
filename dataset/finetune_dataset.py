3bf2a2debea24c77c0aee538c801f0218c4ae28c
zephyr-sh <shaynling@gmail.com>
2025-02-07 14:01:52 +0800

    [A] Add `MIDV2020BenchmarkDataset`

--- dataset/finetune_dataset.py
+++ dataset/finetune_dataset.py
@@ -96,3 +96,68 @@ class MRZFinetuneDataset:
             img = self.to_tensor(img)
 
         return img, gt
+
+
+class MIDV2020BenchmarkDataset:
+
+    def __init__(
+        self,
+        root: str = "/data/Dataset/MIDV2020",
+        return_tensor: bool = False
+    ):
+        self.root = Path(root)
+        self.return_tensor = return_tensor
+
+        ds_midv2020 = []
+        gt_codebook = {
+            'aze_passport': {},
+            'grc_passport': {},
+            'lva_passport': {},
+            'srb_passport': {}
+        }
+
+        for target, val in gt_codebook.items():
+            data = cb.load_json(
+                self.root / f'dataset/templates/annotations/{target}.json')
+            for d in data['_via_img_metadata'].values():
+                for region in d['regions']:
+                    if region['region_attributes']['field_name'] == 'mrz_line0':
+                        mrz1 = region['region_attributes']['value']
+                    if region['region_attributes']['field_name'] == 'mrz_line1':
+                        mrz2 = region['region_attributes']['value']
+                val[d['filename'].replace('.jpg', '')] = mrz1 + '&' + mrz2
+
+        for f in cb.Tqdm(cb.get_files(self.root / 'dataset/images/', suffix=['.jpg'])):
+            if 'aze_passport' in str(f):
+                target = 'aze_passport'
+            elif 'grc_passport' in str(f):
+                target = 'grc_passport'
+            elif 'lva_passport' in str(f):
+                target = 'lva_passport'
+            elif 'srb_passport' in str(f):
+                target = 'srb_passport'
+            else:
+                continue
+
+            key = f.stem if len(f.stem) == 2 else f.parent.stem
+            gt = gt_codebook[target][key]
+
+            ds_midv2020.append((f, gt))
+
+        self.ds_midv2020 = ds_midv2020
+
+    def to_tensor(self, img: np.ndarray) -> np.ndarray:
+        img = img.transpose(2, 0, 1).astype(np.float32) / 255.
+        return img
+
+    def __len__(self):
+        return len(self.ds_midv2020)
+
+    def __getitem__(self, idx):
+        img_path, gt = self.ds_midv2020[idx]
+        img = cb.imread(img_path)
+
+        if self.return_tensor:
+            img = self.to_tensor(img)
+
+        return img, gt
